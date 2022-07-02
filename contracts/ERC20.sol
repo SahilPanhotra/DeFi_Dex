967bf6102d0f81e0b780d8d9ea22aa9e7af38740
SahilPanhotra <sahilpanhotra@gmail.com>
2022-07-02 23:20:50 +0530

    Added liquidity

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -9,6 +9,63 @@ contract Exchange is ERC20 {
          require(_CryptoDevToken != address(0), "Token address passed is a null address");
          cryptoDevTokenAddress = _CryptoDevToken;
     }
-   
+   /**
+* @dev Returns the amount of `Crypto Dev Tokens` held by the contract
+*/
+function getReserve() public view returns (uint) {
+    return ERC20(cryptoDevTokenAddress).balanceOf(address(this));
+}
+/**
+* @dev Adds liquidity to the exchange.
+*/
+function addLiquidity(uint _amount) public payable returns (uint) {
+    uint liquidity;
+    uint ethBalance = address(this).balance;
+    uint cryptoDevTokenReserve = getReserve();
+    ERC20 cryptoDevToken = ERC20(cryptoDevTokenAddress);
+    /*
+        If the reserve is empty, intake any user supplied value for
+        `Ether` and `Crypto Dev` tokens because there is no ratio currently
+    */
+    if(cryptoDevTokenReserve == 0) {
+        // Transfer the `cryptoDevToken` from the user's account to the contract
+        cryptoDevToken.transferFrom(msg.sender, address(this), _amount);
+        // Take the current ethBalance and mint `ethBalance` amount of LP tokens to the user.
+        // `liquidity` provided is equal to `ethBalance` because this is the first time user
+        // is adding `Eth` to the contract, so whatever `Eth` contract has is equal to the one supplied
+        // by the user in the current `addLiquidity` call
+        // `liquidity` tokens that need to be minted to the user on `addLiquidity` call should always be proportional
+        // to the Eth specified by the user
+        liquidity = ethBalance;
+        _mint(msg.sender, liquidity);
+        // _mint is ERC20.sol smart contract function to mint ERC20 tokens
+    } else {
+        /*
+            If the reserve is not empty, intake any user supplied value for
+            `Ether` and determine according to the ratio how many `Crypto Dev` tokens
+            need to be supplied to prevent any large price impacts because of the additional
+            liquidity
+        */
+        // EthReserve should be the current ethBalance subtracted by the value of ether sent by the user
+        // in the current `addLiquidity` call
+        uint ethReserve =  ethBalance - msg.value;
+        // Ratio should always be maintained so that there are no major price impacts when adding liquidity
+        // Ratio here is -> (cryptoDevTokenAmount user can add/cryptoDevTokenReserve in the contract) = (Eth Sent by the user/Eth Reserve in the contract);
+        // So doing some maths, (cryptoDevTokenAmount user can add) = (Eth Sent by the user * cryptoDevTokenReserve /Eth Reserve);
+        uint cryptoDevTokenAmount = (msg.value * cryptoDevTokenReserve)/(ethReserve);
+        require(_amount >= cryptoDevTokenAmount, "Amount of tokens sent is less than the minimum tokens required");
+        // transfer only (cryptoDevTokenAmount user can add) amount of `Crypto Dev tokens` from users account
+        // to the contract
+        cryptoDevToken.transferFrom(msg.sender, address(this), cryptoDevTokenAmount);
+        // The amount of LP tokens that would be sent to the user should be propotional to the liquidity of
+        // ether added by the user
+        // Ratio here to be maintained is ->
+        // (LP tokens to be sent to the user (liquidity)/ totalSupply of LP tokens in contract) = (Eth sent by the user)/(Eth reserve in the contract)
+        // by some maths -> liquidity =  (totalSupply of LP tokens in contract * (Eth sent by the user))/(Eth reserve in the contract)
+        liquidity = (totalSupply() * msg.value)/ ethReserve;
+        _mint(msg.sender, liquidity);
+    }
+     return liquidity;
+}
 
 }
